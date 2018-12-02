caa2b53868eac6a6f8a650ec64ed731e944102bb
RedPanda4552 <redpanda4552@gmail.com>
2018-12-02 04:31:43 -0500

    Add some notice about reloading

--- src/main/java/io/github/redpanda4552/HifumiBot/command/CommandDynCmd.java
+++ src/main/java/io/github/redpanda4552/HifumiBot/command/CommandDynCmd.java
@@ -71,7 +71,7 @@ public class CommandDynCmd extends AbstractCommand {
             String helpText = stringify(args);
             
             if (hifumiBot.getDynamicCommandLoader().insertCommand(name, helpText)) {
-                hifumiBot.sendMessage(channel, "Successfully created empty command `" + name + "`.");
+                hifumiBot.sendMessage(channel, "Successfully created empty command `" + name + "`. Changes will apply after the next `>reload`.");
             } else {
                 hifumiBot.sendMessage(channel, "Failed to create command `" + name + "`.");
             }
@@ -87,7 +87,7 @@ public class CommandDynCmd extends AbstractCommand {
             String value = args[3];
             
             if (hifumiBot.getDynamicCommandLoader().updateCommand(name, attribute, value)) {
-                hifumiBot.sendMessage(channel, "Successfully updated command `" + name + "`.");
+                hifumiBot.sendMessage(channel, "Successfully updated command `" + name + "`. Changes will apply after the next `>reload`.");
             } else {
                 hifumiBot.sendMessage(channel, "Failed to update command `" + name + "`.");
             }
@@ -95,7 +95,7 @@ public class CommandDynCmd extends AbstractCommand {
             break;
         case "remove":
             if (hifumiBot.getDynamicCommandLoader().removeCommand(name)) {
-                hifumiBot.sendMessage(channel, "Successfully removed command `" + name + "`.");
+                hifumiBot.sendMessage(channel, "Successfully removed command `" + name + "`. Changes will apply after the next `>reload`.");
             } else {
                 hifumiBot.sendMessage(channel, "Failed to remove command `" + name + "`.");
             }
