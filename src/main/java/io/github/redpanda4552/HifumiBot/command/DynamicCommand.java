848e5f3e1cb41902d333c291bfde48803889f504
RedPanda4552 <redpanda4552@gmail.com>
2020-12-28 20:55:16 -0500

    Add option to echo back dynamic command contents

--- src/main/java/io/github/redpanda4552/HifumiBot/command/DynamicCommand.java
+++ src/main/java/io/github/redpanda4552/HifumiBot/command/DynamicCommand.java
@@ -61,24 +61,44 @@ public class DynamicCommand extends AbstractCommand {
         return helpText;
     }
     
+    public void setHelpText(String helpText) {
+        this.helpText = helpText;
+    }
+    
+    public String getCategory() {
+        return this.category;
+    }
+    
     public void setCategory(String category) {
         this.category = category;
+    }
+    
+    public boolean getAdmin() {
+        return this.admin;
     }
     
     public void setAdmin(boolean admin) {
         this.admin = admin;
     }
     
-    public void setHelpText(String helpText) {
-        this.helpText = helpText;
+    public String getTitle() {
+        return this.title;
     }
     
     public void setTitle(String title) {
         this.title = title;
     }
     
+    public String getBody() {
+        return this.body;
+    }
+    
     public void setBody(String body) {
         this.body = body;
+    }
+    
+    public String getImageURL() {
+        return this.imageURL;
     }
     
     public void setImageURL(String imageURL) {
