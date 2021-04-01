8ecca00f55edf7c8e6e697ab16e4f942e5962116
RedPanda4552 <redpanda4552@gmail.com>
2021-03-31 21:51:12 -0400

    Fix dev accidentally being restricted channel

--- src/main/java/io/github/redpanda4552/HifumiBot/command/commands/CommandDev.java
+++ src/main/java/io/github/redpanda4552/HifumiBot/command/commands/CommandDev.java
@@ -34,7 +34,7 @@ public class CommandDev extends AbstractCommand {
     private final String DEV_CHANNEL = "dev-builds";
     
     public CommandDev() {
-        super("dev", CATEGORY_BUILTIN, false, true);
+        super("dev", CATEGORY_BUILTIN, false, false);
     }
 
     @Override
