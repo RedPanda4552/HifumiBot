2d5fe4f53dcb8d84732867240e2ad7f245fec063
RedPanda4552 <redpanda4552@gmail.com>
2021-07-31 21:19:09 -0400

    Temporary copy

--- src/main/java/io/github/redpanda4552/HifumiBot/HifumiBot.java
+++ src/main/java/io/github/redpanda4552/HifumiBot/HifumiBot.java
@@ -126,10 +126,12 @@ public class HifumiBot
         
         WarezTrackingManager.createIfNotExists();
         warezTracking = WarezTrackingManager.read();
+        warezTracking.warezUsers = config.warezUsers;
         WarezTrackingManager.write(warezTracking);
         
         DynCmdConfigManager.createIfNotExists();
         dynCmdConfig = DynCmdConfigManager.read();
+        dynCmdConfig.dynamicCommands = config.dynamicCommands;
         DynCmdConfigManager.write(dynCmdConfig);
         
         scheduler = new Scheduler();
