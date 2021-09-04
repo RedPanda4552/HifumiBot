7114ec4f49db43eacbbed32ffdf14b7b2524efc2
RedPanda4552 <redpanda4552@gmail.com>
2021-09-04 14:03:54 -0400

    Try-catch wrap

--- src/main/java/io/github/redpanda4552/HifumiBot/command/commands/CommandWarez.java
+++ src/main/java/io/github/redpanda4552/HifumiBot/command/commands/CommandWarez.java
@@ -40,44 +40,45 @@ public class CommandWarez extends AbstractCommand {
 
     @Override
     public void execute(CommandMeta cm) {
-        if (!(cm.getChannel() instanceof TextChannel))
-            return;
+        try {
+            if (!(cm.getChannel() instanceof TextChannel))
+                return;
 
-        EmbedBuilder eb = EmbedUtil.newFootedEmbedBuilder(cm.getMember());
-        eb.setTitle("PCSX2 Anti-Warez Rules");
-        eb.setDescription("As per ");
-        TextChannel welcomeRules = cm.getGuild()
-                .getTextChannelById(HifumiBot.getSelf().getConfig().channels.rulesChannelId);
-        eb.appendDescription(welcomeRules.getAsMention())
-                .appendDescription(", our server **does not support** piracy.\n")
-                .appendDescription(
-                        "- No help or support will be given to anyone who uses pirated games, BIOS files, or other materials\n")
-                .appendDescription("- Do not discuss how to pirate games, BIOS files, or other materials\n")
-                .appendDescription("- Do not discuss why you think piracy should be allowed\n");
-        eb.addField("Enforcement", "Enforcement is at the discretion of server staff, as they see fit.", false);
-        eb.addField("Appeal",
-                "You may appeal a warez tag by proving that you own the item in question and disposing of any pirated copies. Staff will want to see some visual proof that you own the item (E.g. A picture of the item with your Discord username and the current date on a sticky note next to it).",
-                false);
-        eb.addField("Your Stance On Piracy",
-                "We are not here to argue. Please forward all complaints about copyright law to Sony, or your local government's copyright enforcement agency.",
-                false);
-        eb.addField("\"But a friend gave it to me!\" or \"I own a copy, and just downloaded it instead!\"",
-                "Games, BIOS files and other materials must be from discs, a console or other device that you own.",
-                false);
-        Messaging.sendMessageEmbed(cm.getChannel(), eb.build());
+            TextChannel welcomeRules = cm.getGuild().getTextChannelById(HifumiBot.getSelf().getConfig().channels.rulesChannelId);
+            EmbedBuilder eb = EmbedUtil.newFootedEmbedBuilder(cm.getMember());
+            eb.setTitle("PCSX2 Anti-Warez Rules");
+            eb.setDescription("As per ")
+                    .appendDescription(welcomeRules.getAsMention())
+                    .appendDescription(", our server **does not support** piracy.\n")
+                    .appendDescription("- No help or support will be given to anyone who uses pirated games, BIOS files, or other materials\n")
+                    .appendDescription("- Do not discuss how to pirate games, BIOS files, or other materials\n")
+                    .appendDescription("- Do not discuss why you think piracy should be allowed\n");
+            eb.addField("Enforcement", "Enforcement is at the discretion of server staff, as they see fit.", false);
+            eb.addField(
+                    "Appeal",
+                    "You may appeal a warez tag by proving that you own the item in question and disposing of any pirated copies. Staff will want to see some visual proof that you own the item (E.g. A picture of the item with your Discord username and the current date on a sticky note next to it).",
+                    false);
+            eb.addField(
+                    "Your Stance On Piracy",
+                    "We are not here to argue. Please forward all complaints about copyright law to Sony, or your local government's copyright enforcement agency.",
+                    false);
+            eb.addField(
+                    "\"But a friend gave it to me!\" or \"I own a copy, and just downloaded it instead!\"",
+                    "Games, BIOS files and other materials must be from discs, a console or other device that you own.",
+                    false);
+            Messaging.sendMessageEmbed(cm.getChannel(), eb.build());
 
-        for (Member member : cm.getMentions()) {
-            if (!HifumiBot.getSelf().getPermissionManager().hasPermission(PermissionLevel.MOD, member)) {
-                try {
-                    cm.getGuild()
-                            .addRoleToMember(member,
-                                    cm.getGuild().getRoleById(HifumiBot.getSelf().getConfig().roles.warezRoleId))
-                            .complete();
-                } catch (InsufficientPermissionException e) {
-                    Messaging.logInfo("CommandWarez", "execute",
-                            "Failed to assign role to " + member.getAsMention() + " (insufficient permissions)");
+            for (Member member : cm.getMentions()) {
+                if (!HifumiBot.getSelf().getPermissionManager().hasPermission(PermissionLevel.MOD, member)) {
+                    try {
+                        cm.getGuild().addRoleToMember(member, cm.getGuild().getRoleById(HifumiBot.getSelf().getConfig().roles.warezRoleId)).complete();
+                    } catch (InsufficientPermissionException e) {
+                        Messaging.logInfo("CommandWarez", "execute", "Failed to assign role to " + member.getAsMention() + " (insufficient permissions)");                        
+                    }
                 }
             }
+        } catch (Exception e) {
+            Messaging.logException("CommandWarez", "execute", e);
         }
     }
 
