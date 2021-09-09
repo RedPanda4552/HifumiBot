4ba49c100fd7ce11390ec70916927bb180367ef6
RedPanda4552 <redpanda4552@gmail.com>
2021-09-09 11:32:24 -0400

    Implement configurable dev build instructions

--- src/main/java/io/github/redpanda4552/HifumiBot/command/CommandIndex.java
+++ src/main/java/io/github/redpanda4552/HifumiBot/command/CommandIndex.java
@@ -45,6 +45,7 @@ import io.github.redpanda4552.HifumiBot.command.commands.CommandPerms;
 import io.github.redpanda4552.HifumiBot.command.commands.CommandRun;
 import io.github.redpanda4552.HifumiBot.command.slash.CommandAbout;
 import io.github.redpanda4552.HifumiBot.command.slash.CommandBan;
+import io.github.redpanda4552.HifumiBot.command.slash.CommandDevPrompt;
 import io.github.redpanda4552.HifumiBot.command.slash.CommandReload;
 import io.github.redpanda4552.HifumiBot.command.slash.CommandSay;
 import io.github.redpanda4552.HifumiBot.command.slash.CommandShutdown;
@@ -88,6 +89,7 @@ public class CommandIndex {
         registerSlashCommand(new CommandReload());
         registerSlashCommand(new CommandWiki());
         registerSlashCommand(new CommandBan());
+        registerSlashCommand(new CommandDevPrompt());
         
         commandMap.clear();
         CommandCPU cpu = new CommandCPU();
