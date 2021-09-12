5e7ed63f225754b6c30dff6e4daf9c2fc8970228
RedPanda4552 <redpanda4552@gmail.com>
2021-09-12 16:24:14 -0400

    Port run to slash command

---
+++ src/main/java/io/github/redpanda4552/HifumiBot/command/slash/CommandRun.java
@@ -0,0 +1,64 @@
+/**
+ * This file is part of HifumiBot, licensed under the MIT License (MIT)
+ * 
+ * Copyright (c) 2020 RedPanda4552 (https://github.com/RedPanda4552)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.github.redpanda4552.HifumiBot.command.slash;
+
+import io.github.redpanda4552.HifumiBot.HifumiBot;
+import io.github.redpanda4552.HifumiBot.command.AbstractSlashCommand;
+import io.github.redpanda4552.HifumiBot.permissions.PermissionLevel;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+public class CommandRun extends AbstractSlashCommand {
+    
+    public CommandRun() {
+        super(PermissionLevel.SUPER_ADMIN);
+    }
+
+    @Override
+    protected void onExecute(SlashCommandEvent event) {
+        String runnableName = event.getOption("runnable").getAsString();
+        boolean result = HifumiBot.getSelf().getScheduler().runScheduledNow(runnableName);
+        
+        if (result) {
+            event.reply("Sent an execute request for runnable '" + runnableName + "' to the thread pool; it will run whenever a thread is available to host it.").queue();
+        } else {
+            event.reply("Something went wrong while trying to add the runnable to the thread pool; try again in a few moments.").setEphemeral(true).queue();
+        }
+    }
+
+    @Override
+    protected CommandData defineSlashCommand() {
+        OptionData runnable = new OptionData(OptionType.STRING, "runnable", "Name of the runnable to execute").setRequired(true);
+        
+        for (String runnableName : HifumiBot.getSelf().getScheduler().getRunnableNames()) {
+            runnable.addChoice(runnableName, runnableName);
+        }
+        
+        return new CommandData("run", "Execute a scheduled runnable immediately")
+                .addOptions(runnable);
+    }
+
+}
