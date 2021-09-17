ab568b6949d064b14a7d8aaef3f6c3e230159a1d
RedPanda4552 <redpanda4552@gmail.com>
2021-09-17 18:14:27 -0400

    Soup up the regexes and replies

--- src/main/java/io/github/redpanda4552/HifumiBot/util/PixivSourceFetcher.java
+++ src/main/java/io/github/redpanda4552/HifumiBot/util/PixivSourceFetcher.java
@@ -26,6 +26,7 @@ package io.github.redpanda4552.HifumiBot.util;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,40 +34,69 @@ import io.github.redpanda4552.HifumiBot.HifumiBot;
 import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Message.Attachment;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.Button;
 
 public class PixivSourceFetcher {
 
     private static final String PIXIV_BASE_URL = "https://www.pixiv.net/artworks/";
-    private static final String PIXIV_PATTERN = "([0-9]+)";
-    private static final Pattern p = Pattern.compile(PIXIV_PATTERN);
+    private static final String[] PIXIV_PATTERN = { ".*illust_([0-9]+)_.+", "[a-zA-Z_]*([0-9]+)_p[0-9]+.+" };
+    private static final Pattern[] p = { Pattern.compile(PIXIV_PATTERN[0]), Pattern.compile(PIXIV_PATTERN[1]) };
     
     public static void getPixivLink(Message message) {
         if (!message.getChannel().getId().equals(HifumiBot.getSelf().getConfig().channels.pixivChannelId)) {
             return;
         }
         
+        ArrayList<String> imageUrls = new ArrayList<String>();
         MessageBuilder mb = new MessageBuilder();
         
         for (Attachment attach : message.getAttachments()) {
-            Matcher m = p.matcher(attach.getFileName());
+            int pos = 0;
+            Matcher m = p[pos].matcher(attach.getFileName());
             
-            while (m.find()) {
-                try {
-                    URL url = new URL(PIXIV_BASE_URL + m.group());
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.connect();
-                    
-                    if (connection.getResponseCode() == 200) {
-                        mb.append("<" + PIXIV_BASE_URL + m.group() + ">\n");
-                        break;
-                    }
-                } catch (IOException e) { }
+            while (!m.matches()) {
+                if (++pos >= p.length) {
+                    return;
+                }
+                
+                m = p[pos].matcher(attach.getFileName());
+            }
+            
+            try {
+                URL url = new URL(PIXIV_BASE_URL + m.group(1));
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+                
+                if (connection.getResponseCode() == 200) {
+                    imageUrls.add(PIXIV_BASE_URL + m.group(1));
+                    break;
+                }
+            } catch (IOException e) { }
+        }
+        
+        for (int i = 0; i < imageUrls.size(); i++) {
+            if (message.getContentDisplay().contains(imageUrls.get(i))) {
+                imageUrls.remove(i--);
             }
         }
         
-        if (!mb.isEmpty()) {
-            Messaging.sendMessage(message.getChannel(), mb.build());
+        if (!imageUrls.isEmpty()) {
+            mb.append("Found the sauce! ");
+            
+            if (imageUrls.size() > 1) {
+                mb.append("(button order matches image order)");
+            }
+            
+            ArrayList<Button> buttons = new ArrayList<Button>();
+            
+            for (String imageUrl : imageUrls) {
+                buttons.add(Button.link(imageUrl, "Go to Pixiv"));
+            }
+            
+            mb.setActionRows(ActionRow.of(buttons));
+            message.reply(mb.build()).mentionRepliedUser(false).queue();
         }
     }
 }
