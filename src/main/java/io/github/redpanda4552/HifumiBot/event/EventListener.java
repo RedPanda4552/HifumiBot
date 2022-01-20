fd3acc2e7c9e05788906abdc8a43f1e99da11d99
RedPanda4552 <redpanda4552@gmail.com>
2022-01-20 10:55:37 -0500

    Message received events fire when WE send DMs? So filter us out.

--- src/main/java/io/github/redpanda4552/HifumiBot/event/EventListener.java
+++ src/main/java/io/github/redpanda4552/HifumiBot/event/EventListener.java
@@ -73,7 +73,10 @@ public class EventListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getChannelType() == ChannelType.PRIVATE) {
-            Messaging.logInfo("EventListener", "onMessageReceived", "DM sent to Hifumi by user " + event.getAuthor().getAsMention() + " (" + event.getAuthor().getAsTag() + ")\n\n```\n" + StringUtils.truncate(event.getMessage().getContentRaw(), 500) + "\n```\nMessage content displayed raw format, truncated to 500 chars. Original length: " + event.getMessage().getContentRaw().length());
+            if (!event.getAuthor().getId().equals(HifumiBot.getSelf().getJDA().getSelfUser().getId())) {
+                Messaging.logInfo("EventListener", "onMessageReceived", "DM sent to Hifumi by user " + event.getAuthor().getAsMention() + " (" + event.getAuthor().getAsTag() + ")\n\n```\n" + StringUtils.truncate(event.getMessage().getContentRaw(), 500) + "\n```\nMessage content displayed raw format, truncated to 500 chars. Original length: " + event.getMessage().getContentRaw().length());
+            }
+            
             return;
         }
         
