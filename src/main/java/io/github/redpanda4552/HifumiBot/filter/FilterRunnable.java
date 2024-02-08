b6bfe193711ce28d118d5c7faf97a5689e11d7bc
RedPanda4552 <redpanda4552@gmail.com>
2024-02-08 14:40:17 -0500

    Fix inlining on kick/timeout messages

--- src/main/java/io/github/redpanda4552/HifumiBot/filter/FilterRunnable.java
+++ src/main/java/io/github/redpanda4552/HifumiBot/filter/FilterRunnable.java
@@ -38,7 +38,7 @@ public class FilterRunnable implements Runnable {
                         EmbedBuilder eb = new EmbedBuilder();
                         eb.setTitle("User Automatically Kicked");
                         eb.setDescription("User was automatically kicked from the server for repeated filter incidents.");
-                        eb.addField("User (As Mention)", usr.getAsMention(), false);
+                        eb.addField("User (As Mention)", usr.getAsMention(), true);
                         eb.addField("Username", usr.getName(), true);
                         eb.addField("User ID", usr.getId(), true);
                         eb.setColor(Color.YELLOW);
@@ -59,7 +59,7 @@ public class FilterRunnable implements Runnable {
                     EmbedBuilder eb = new EmbedBuilder();
                     eb.setTitle("User Automatically Timed Out");
                     eb.setDescription("User was automatically timed out for spam.");
-                    eb.addField("User (As Mention)", usr.getAsMention(), false);
+                    eb.addField("User (As Mention)", usr.getAsMention(), true);
                     eb.addField("Username", usr.getName(), true);
                     eb.addField("User ID", usr.getId(), true);
                     eb.setColor(Color.YELLOW);
